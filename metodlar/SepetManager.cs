9edf9c5296e68a09bbb0e992c8a346d17d178943
Melisa <meliisaustun@gmail.com>
2021-02-05 00:30:50 +0300

    16 Ocak Ders Sonu

---
+++ metodlar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) //class
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        { //classdeğil
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + urunAdi);
+        }
+    }
+}
